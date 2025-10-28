461cb428855c78453108b8c58fd8d8ad4aa8aa05
pooja02082020 <lohanipooja77@gmail.com>
2025-10-27 21:55:54 -0400

    datatypes

---
+++ operators.js
@@ -0,0 +1,60 @@
+// let a=12;
+// let b=5;
+// 
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);  //remainder modulus
+// console.log(a**b);  //a to poser b 12^5
+
+let a=10;
+// a++;
+// console.log(a++);  //output is 10 even a++ after the value it will use the value and then then change the value, prints 10 and then a becomes 11
+// console.log(a);
+// a--;
+// console.log(a);
+//increment - add 1 to a number (++)
+//decrement - add subtract 1 from a number (--\)
+
+console.log(++a);
+
+let x=5;
+console.log(x++);
+console.log(--x);
+
+let str = "23";
+console.log(typeof(str));
+let b = +str;
+console.log(typeof(b));
+
+console.log(+true);
+
+
+//AssiGNMENT OPERATORS
+let a1=10;
+//a =a+5;
+console.log(a1+=5);
+//console.log(a1);
+
+//comparison operator
+let p=10;
+let q=20;
+let r="10"
+console.log(p>q);
+console.log(p<q);
+console.log(p>=q);
+console.log(p<=q);
+console.log(p==q);
+console.log(p != q);
+console.log(p==r); //true = it doesnot matter whether its int or string only compares the number
+console.log(p===r);
+console.log(p != r); //LOOSE COMPARISON -FALSE
+console.log(p !== r); //STRICT COMPARISON - TRUE
+
+console.log(p+r); //1010 STRING + NUMBER -STRING CONCATENATION
+console.log(p-r); //0 
+console.log(p*r);
+console.log(p/r);
+console.log(p%r);
+
