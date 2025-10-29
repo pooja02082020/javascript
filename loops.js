04ce585c286326fbc983e219397234c4bcd8284e
pooja02082020 <lohanipooja77@gmail.com>
2025-10-28 20:52:18 -0400

    loops

--- loops.js
+++ loops.js
@@ -1,4 +1,46 @@
-for (let i = 0; i < 10; i++) {
-    console.log("Hello World!");
-}
+// for (let i = 0; i < 10; i++) {
+    // console.log(i);
+// }
+// 
+// for (let i = 0; i < 10; i++) {
+//    console.log("hello world");
+// }
 //for (initialization;Condition;increment/updation)
+
+//Calculate the sum of first 10 numbers
+let sum =0;
+for(let i=1;i<=10;i++){
+    sum+=i; //sum = sum+i;
+   // console.log(sum);
+}
+console.log("The sum of the first 10 numbers is ",sum);
+
+
+//while loop
+//syntax
+// let i = 1;
+// while (i <= 10) {
+//   console.log("Hello World");
+//   i++;
+// }
+// 
+//DO WHILE LOOP
+// let i =1;
+// do{
+    // console.log("Hello World"); 
+    // i++;
+// }while (i <= 10)
+
+
+
+    let i =1;
+do{
+    console.log("Hello World");
+    i++;
+}while (i <= 0)  //even if condition is false the code will run atleast once before checking the condition
+
+//NaN
+let a="something";
+console.log(a/2);
+
+console.log(typeof(a/2)); //NaN itself is a type of number
