fff52dd31f46505df13e218ad8c72be30f1611d0
pooja02082020 <lohanipooja77@gmail.com>
2025-10-30 21:56:03 -0400

    DOM objects

--- DOM.js
+++ DOM.js
@@ -1,5 +1,27 @@
-console.log(window)  //browser automatically creates window object
-window.console.log("Hello");
-window.alert("Hello");
+// console.log(window)  //browser automatically creates window object
+// window.console.log("Hello");
+// window.alert("Hello");
 
-//console.dir(window.document)
+// console.dir(window.document)
+
+const heading = document.getElementById("heading");
+console.log(heading)
+
+let boxes = document.getElementsByClassName("boxes");
+console.log(boxes)
+
+let getByTags = document.getElementsByTagName("div");
+console.log(getByTags)
+
+let div = document.querySelector(".boxes");  //for getting classname
+//let div = document.querySelector("div");  //for getting tagname
+console.log(div)
+
+//query selector all
+let divs = document.querySelectorAll(".boxes");
+console.log(divs);
+
+//DOM properties
+//innerText
+let para =document.querySelector("#ptag")
+console.log(para.innerText);
